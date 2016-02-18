39f996b5247cb0cbb7e204a97c6930d5de8d8a50
mark <nothing>
2016-02-18 09:38:38 +0800

    override toString method

--- ms-commons/commons-dao/src/main/java/com/ms/framework/dao/core/paging/PageImpl.java
+++ ms-commons/commons-dao/src/main/java/com/ms/framework/dao/core/paging/PageImpl.java
@@ -1,10 +1,13 @@
 package com.ms.framework.dao.core.paging;
 
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-/** EL表达式无法获取is和has开头的布尔值
+/**
+ * EL表达式无法获取is和has开头的布尔值
  * Created by mark.zhu on 2016/2/16.
  */
 public class PageImpl<T> extends org.springframework.data.domain.PageImpl<T> {
@@ -30,5 +33,21 @@ public class PageImpl<T> extends org.springframework.data.domain.PageImpl<T> {
 
     public boolean getHasNext() {
         return this.hasNext();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + MoreObjects.toStringHelper(this)
+                .add("totalPages", this.getTotalPages())
+                .add("number", this.getNumber())
+                .add("size", this.getSize())
+                .add("isFirst", this.isFirst())
+                .add("isLast", this.isLast())
+                .add("hasPrevious", this.hasPrevious())
+                .add("hasNext", this.hasNext())
+                .add("totalElements", this.getTotalElements())
+                .add("contentSize", this.getContent().size())
+                .add("numberOfElements", this.getNumberOfElements())
+                .toString();
     }
 }
