471a147cc3fcb055f319b7dc109cc5d995b8f226
mark <nothing>
2016-02-18 13:38:34 +0800

    add test paging

--- ms-commons/commons-dao/src/test/java/com/ms/framework/test/dao/UserDaoTest.java
+++ ms-commons/commons-dao/src/test/java/com/ms/framework/test/dao/UserDaoTest.java
@@ -1,6 +1,7 @@
 package com.ms.framework.test.dao;
 
 import com.ms.framework.dao.account.UserDao;
+import com.ms.framework.dao.core.paging.PageImpl;
 import com.ms.framework.dao.core.paging.PagingBounds;
 import com.ms.framework.domain.account.User;
 import org.apache.ibatis.session.RowBounds;
@@ -9,12 +10,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
@@ -27,62 +31,58 @@ import java.util.Random;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:spring/spring-root.xml")
-public class UserDaoTest {
+public class UserDaoTest extends BaseDaoTest<UserDao,User>{
     private static final Logger LOGGER = LoggerFactory.getLogger(UserDaoTest.class);
     @Resource
     private UserDao userDao;
 
-    @Test
+    @Override
+    protected UserDao getDao() {
+        return userDao;
+    }
+
+    @Override
+    protected User getMockedEntity() {
+        User user = new User();
+        user.setLoginName("user1" + new Random().nextInt(Integer.MAX_VALUE));
+        user.setEmail("user@user.com");
+        user.setMobile("187-9987-0099");
+        user.setPassword("pwd");
+        user.setSalt("salt");
+        user.setStatus(User.UserAccountStatus.VALID.getValue());
+        user.setDeleted(User.DELETED_NO);
+        return user;
+    }
+
+    @Override
     public void testCrud() throws Exception {
-        LOGGER.info("test insert...");
-        User user = getUserAccountMock();
-        long affected = userDao.insert(user);
-        LOGGER.info(user.toString());
-        Assert.assertTrue(affected == 1);
-        Assert.assertTrue(user.getId() == 1);
-
-        User result = userDao.getById(1);
-        Assert.assertTrue(result.getId() == 1);
-
-        result = userDao.get(null);
-        Assert.assertTrue(result.getId() == 1);
-        result = userDao.get(new User());
-        Assert.assertTrue(result.getId() == 1);
-
-        String newName = "updated " + new SimpleDateFormat("HHmmss").format(new Date());
-        user.setLoginName(newName);
-        int updateAffected = userDao.update(user);
-        Assert.assertEquals(updateAffected, 1);
-        Assert.assertEquals(userDao.getById(user.getId()).getLoginName(), newName);
-
-        long count = userDao.getCount(null);
-        Assert.assertEquals(count, 1);
-
-        List<User> userList = userDao.getList(null);
-        Assert.assertEquals(userList.size(), 1);
-
-        int deleteAffected = userDao.delete(1);
-        Assert.assertEquals(deleteAffected, 1);
-
-        count = userDao.getCount(null);
-        Assert.assertEquals(count, 0);
-
+        super.doCrud();
     }
 
     @Test
     public void testPage() throws Exception {
+        //prepare data
         for (int i = 0; i < 11; i++) {
-           int affected = userDao.insert(getUserAccountMock());
-        }
-        PagingBounds rowBounds = new PagingBounds(0, 10,true,new PagingBounds.SortEntity("create_time", Sort.Direction.DESC));
-        List<User> userList = userDao.getListPage(rowBounds, null);
-        LOGGER.info(rowBounds.toString());
-        for (User user : userList) {
-            LOGGER.info(user.toString());
+            int affected = userDao.insert(getUserAccountMock());
         }
 
+        int pageSize = 3;
+        PageRequest request = new PageRequest(0, 3, new Sort(Sort.Direction.DESC, "create_time"));
+        long total = userDao.getCount(null);
+        //根据总记录数和pageSize计算总页数
+        Page<User> page = new PageImpl(new ArrayList(), request, total);
+
+        //获取每一页数据
+        for (int i = 0; i < page.getTotalPages(); i++) {
+            PageRequest pageRequest = new PageRequest(i, pageSize, new Sort(Sort.Direction.DESC, "create_time"));
+            List<User> userList = userDao.getListPage(PagingBounds.createPagingBounds(pageRequest), null);
+            Page<User> actualPage = new PageImpl(userList, pageRequest, total);
+            LOGGER.info(actualPage.toString());
+            for (User user : actualPage.getContent()) {
+                LOGGER.info(user.toString());
+            }
+        }
     }
-
     private User getUserAccountMock() {
         User user = new User();
         user.setLoginName("user1" + new Random().nextInt(Integer.MAX_VALUE));
